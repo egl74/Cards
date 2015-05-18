2d519dbfae03ad3fb7622b50be0ee4ab1e9de367
sanchezik <sanchezlife@tut.by>
2015-05-18 16:56:08 +0300

    18.05

--- Cards.Web/Controllers/AccountController.cs
+++ Cards.Web/Controllers/AccountController.cs
@@ -156,6 +156,7 @@ namespace Cards.Web.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    //Hello, world!
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
